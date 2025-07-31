aa93be6b4448efc6622b208f8b97b3e460bb70fe
kevdude82 <altvbusiness@gmail.com>
2025-07-31 08:54:27 -0500

    setup UserRepository.java

--- src/main/java/hnch_app_backend/repository/UserRepository.java
+++ src/main/java/hnch_app_backend/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package hnch_app_backend.repository;
 
-public class UserRepository {
-    
+// this is the repository for saving and finding users
+// it uses spring data jpa to make things easier
+
+import hnch_app_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
