da76e76833788efb75b3bdffe8d6b1589126e968
kevdude82 <altvbusiness@gmail.com>
2025-07-31 08:40:39 -0500

    setup MessageRepository.java

--- src/main/java/hnch_app_backend/repository/MessageRepository.java
+++ src/main/java/hnch_app_backend/repository/MessageRepository.java
@@ -1,5 +1,12 @@
 package hnch_app_backend.repository;
 
-public class MessageRepository {
-    
+// this is the repository for saving and finding the messages
+// it uses spring data jpa to make things easier
+
+import hnch_app_backend.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByUserId(Long userId);
 }
